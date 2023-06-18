1d869bc2c86c4f2e189458a0f9c7a73a2e42b21e
Muhammad Hamza Deewan <hamzadeewan01@gmail.com>
2023-06-18 18:13:18 +0500

    Update treatmentPlan.js

--- src/routes/treatmentPlan.js
+++ src/routes/treatmentPlan.js
@@ -4,6 +4,9 @@ const treatmentPlanController = require("../controllers/treatmentPlan");
 
 // Define routes
 router.post("/", treatmentPlanController.createTreatmentPlan);
+router.get("/patient/:patientid", treatmentPlanController.getTreatmentPlanpatient);
+router.get("/doctor/:doctorid", treatmentPlanController.getTreatmentPlanDoctor);
+
 // router.post('/', treatmentPlanController.addPrice);
 
 module.exports = router;
